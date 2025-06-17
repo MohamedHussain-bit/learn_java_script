b660673c06f26b50ba02ed9be2ead4f76b1f1d6b
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-06-17 22:59:56 +0300

    data type in java script

---
+++ data_types.js
@@ -0,0 +1,19 @@
+/*
+Data types in java script
+String
+Number
+Array
+Object
+Boolean
+Undefined
+Null
+*/
+// todo typeof => to now the date type
+
+console.log(typeof "Hello world")
+console.log(typeof 12345)
+console.log(typeof ["1","2","3","4","5"])
+console.log(typeof {name : "mohamed" , age : 19})
+console.log(typeof true)
+console.log(typeof undefined)
+console.log(typeof null)
