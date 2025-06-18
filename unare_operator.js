461e42bdb52dc06cef3254e6820fe6fd05704adb
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-06-18 12:35:31 +0300

    unare operator

---
+++ unare_operator.js
@@ -0,0 +1,10 @@
+// unare plus and negation operator
+/*
++ bucaise turn string to number no change
+- bucaise turn string to number
+*/
+console.log(100)
+console.log(+"100")
+console.log(100)
+console.log(-"100")
+console.log(-"-100")
