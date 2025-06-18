25bd1dc16ae9a90945d6086ddccaeeec08b6882a
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-06-18 13:50:33 +0300

    add toFixed

--- number_methods.js
+++ number_methods.js
@@ -4,3 +4,7 @@ let number = 100;
 let string = number.toString()
 console.log(string)
 console.log(typeof string)
+// toFixed bucause return lemeted number after .
+let visetor = 1000.123456789
+console.log(visetor.toFixed(2))
+
