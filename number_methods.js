1d7fc4740de00cf74312b0fe39ac3227989c4d68
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-06-18 14:01:01 +0300

    add parseInt

--- number_methods.js
+++ number_methods.js
@@ -1,10 +1,12 @@
 // number methods 
-// toString bucause turn number to string
+// toString() bucause turn number to string
 let number = 100;
-let string = number.toString()
-console.log(string)
-console.log(typeof string)
-// toFixed bucause return lemeted number after .
-let visetor = 1000.123456789
-console.log(visetor.toFixed(2))
-
+let string = number.toString();
+console.log(string);
+console.log(typeof string);
+// toFixed() bucause return lemeted number after .
+let visetor = 1000.123456789;
+console.log(visetor.toFixed(2));
+// parseInt() return number
+let age = "19 from Mohamed";
+console.log(parseInt(age))
