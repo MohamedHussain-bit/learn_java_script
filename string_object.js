420c7029585d22187c2b75ea791bc4ba6c4f84fc
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-06-18 16:18:57 +0300

    add to lower case

--- string_object.js
+++ string_object.js
@@ -4,3 +4,5 @@ let name1 = "  Mohamed  ";
 console.log(name1.trim());
 // toUpperCase() turn the alphbet caracrters to upper case
 console.log(name1.toUpperCase());
+// toLowerCase() turn the alphbet caracrters to lower case
+console.log(name1.toLowerCase());
