14f90ff7a068eff443cfc7a4c7e78885ccae4f3a
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-06-18 22:32:30 +0300

    add if

---
+++ control_flow.js
@@ -0,0 +1,10 @@
+// control flow
+// if condation
+// syntax if(){}
+let nameUser = "Mohamed";
+let age = 19;
+if (age >= 18){
+    return `Welcom ${nameUser}`
+}else {
+    return `sore ${nameUser}`
+}
