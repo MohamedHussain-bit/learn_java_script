8a9ed72c96b32f3a824e994ee676044367dc3eea
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-06-18 15:20:51 +0300

    add max

--- math_object.js
+++ math_object.js
@@ -5,5 +5,8 @@ console.log(Math.round(99.5));
 console.log(Math.ceil(99.1));
 // Math.floor() round the number in the botum
 console.log(Math.floor(100.9));
-// Math.min() return minnumber
+// Math.min() return min number
 console.log(Math.min(1,2,3,4,5,6));
+// Math.max() return max number
+console.log(Math.max(1,2,3,4,5,6));
+
