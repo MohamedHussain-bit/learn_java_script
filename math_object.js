0e4498e047250632f5920cabfa819f248ddee25e
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-06-18 15:18:51 +0300

    add min

--- math_object.js
+++ math_object.js
@@ -1,7 +1,9 @@
 // math object
 // Math.round() round the number
-console.log(Math.round(99.5))
+console.log(Math.round(99.5));
 // Math.cile() round the number in the top
-console.log(Math.ceil(99.1))
+console.log(Math.ceil(99.1));
 // Math.floor() round the number in the botum
-console.log(Math.floor(100.9))
+console.log(Math.floor(100.9));
+// Math.min() return minnumber
+console.log(Math.min(1,2,3,4,5,6));
