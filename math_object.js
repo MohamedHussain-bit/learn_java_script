12127e42a880e9cc26504597e8ddeca4d7401254
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-06-18 15:16:12 +0300

    add math floor

--- math_object.js
+++ math_object.js
@@ -3,3 +3,5 @@
 console.log(Math.round(99.5))
 // Math.cile() round the number in the top
 console.log(Math.ceil(99.1))
+// Math.floor() round the number in the botum
+console.log(Math.floor(100.9))
