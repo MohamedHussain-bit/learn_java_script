37438b7065dbc304bbef61b5c1e0481795bb8abf
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-06-18 16:17:28 +0300

    add to upper case

--- string_object.js
+++ string_object.js
@@ -2,3 +2,5 @@
 // trim() remove the space from the string
 let name1 = "  Mohamed  ";
 console.log(name1.trim());
+// toUpperCase() turn the alphbet caracrters to upper case
+console.log(name1.toUpperCase());
