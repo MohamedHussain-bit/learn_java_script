2af0396363ffb943c2ac18a0c6f6ea76256dbb36
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-06-18 11:54:41 +0300

    templet leterals in java script

---
+++ templeat_literals.js
@@ -0,0 +1,8 @@
+// templet leterals
+// syntax `write string ${variable}`
+let name1 = "Mohamed"
+let name2 = "Hussain"
+function sayHello(){
+    return `Hello ${name1} ${name2}`
+}
+console.log(sayHello())
