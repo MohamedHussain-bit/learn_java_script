5b8999986a856a806afb5383c8753eb1783967fc
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-06-19 21:37:03 +0300

    array methods

---
+++ array_methods.js
@@ -0,0 +1,6 @@
+// array methods
+let user = ["Ahmed" , "Mohamed" , "Osama" , "Marawan" , "Ali"]
+console.log(user)
+// unshift() add element to the first
+user.unshift("Amir" , "Nabile")
+console.log(user)
