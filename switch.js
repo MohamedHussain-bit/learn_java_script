acb94197a46561859188e8f463102f74c7a2e5bd
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-06-19 20:36:05 +0300

    add switch

---
+++ switch.js
@@ -0,0 +1,12 @@
+// switch() use to condetion
+let day = 1;
+switch(day){
+    case 1:
+        console.log("saterday")
+    break;
+    case 2:
+        console.log("sunday")
+    break;
+    default:
+        console.log("undefind")
+};
