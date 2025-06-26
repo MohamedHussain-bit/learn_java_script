f41d729ed2643baf7a126962ff8c071684683bdb
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-06-26 22:49:51 +0300

    map

---
+++ new_map.js
@@ -0,0 +1,8 @@
+// Map()
+let myMap = new Map();
+myMap.set("age" , "string")
+myMap.set(10 , "number key")
+myMap.set(true , "bollen key")
+console.log(myMap.get("age"))
+console.log(myMap.get(10))
+console.log(myMap.get(true))
