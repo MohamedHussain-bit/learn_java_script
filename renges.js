658cb2981aee6b73abcaecd85f6ffe0581476fb1
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-06-28 12:38:13 +0300

    add  renges

--- renges.js
+++ renges.js
@@ -3,3 +3,7 @@
 let tld = "Com info Web li"
 let tldRe = /com|li|web/ig
 console.log(tld.match(tldRe))
+// [] => renge to from
+let num = "12345678910"
+let numRe = /[0-5]/g
+console.log(num.match(numRe))
