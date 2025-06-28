b16bd95b7371feab61a0bb6d1d09aff5b6f8a333
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-06-28 12:19:25 +0300

    add golobal

--- regular_expretion.js
+++ regular_expretion.js
@@ -4,3 +4,8 @@ let cong = "Hello Elzero welcom to elzero"
 // i => ignor case latter
 let search = /elzero/i
 console.log(cong.match(search))
+// match() match tow string
+// g => global
+let cong2 = "Hello Elzero welcom to elzero"
+let search2 = /elzero/ig
+console.log(cong2.match(search2))
