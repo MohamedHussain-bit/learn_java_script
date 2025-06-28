72fe21d07907f17373470f162e1e4c8af9be580d
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-06-28 13:31:14 +0300

    add

--- renges.js
+++ renges.js
@@ -11,3 +11,7 @@ console.log(num.match(numRe))
 let num2 = "12345678910"
 let numRe2 = /[^0-5]/g
 console.log(num2.match(numRe2))
+// betwen
+let string = "os os5 os1os os8os"
+let stringRe = /os[5-9]os/ig
+console.log(string.match(stringRe))
