5fc68676bfaa4f1360ebe2c937718b8fe2de8e02
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-06-30 21:16:35 +0300

    import and export

---
+++ export.js
@@ -0,0 +1,7 @@
+// export modules
+export let array = [1,2,3,4,5]
+export let nameUser = "Mohamed"
+
+export function sayHello(){
+    return `Hello ${nameUser}`
+}
---
+++ import.js
@@ -0,0 +1,5 @@
+// import modules
+import { array , nameUser , sayHello as s } from "./export.js";
+console.log(array)
+console.log(nameUser)
+console.log(s())
