1d8b61981de0fd2a558e617475293a2a4b27d5f5
MohamedHussain-bit <godzela.1242006@gmail.com>
2025-07-01 12:52:33 +0300

    turn object to json

--- turn_object_to_json.js
+++ turn_object_to_json.js
@@ -4,3 +4,9 @@ const myJson = '{"userName" : "Mohamed","age": 19}'
 const myobject = JSON.parse(myJson)
 console.log(myJson)
 console.log(myobject)
+
+myobject["userName"] = "Mena"
+myobject["age"] = 20
+
+const myJsonToServer = JSON.stringify(myobject)
+console.log(myJsonToServer)
